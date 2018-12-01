4a77b616605753248ce81ae933f2ecd3213206f8
Your Name <you@example.com>
2018-12-01 14:03:42 +0000

    Homing player and explosion effects (partial).

---
+++ DAMP/Assets/HomingPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingPlayer : MonoBehaviour {
+
+    public Transform target;
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+    //public float thrust = 10f;
+    public GameObject explosionEffect;
+
+
+    // Use this for initialization
+    void Start () {
+        //target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+
+        float rotateAngle = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotateAngle * rotateSpeed;
+
+
+        rb.velocity = transform.up * speed;
+	}
+
+   void OnTriggerEnter2D(){
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
