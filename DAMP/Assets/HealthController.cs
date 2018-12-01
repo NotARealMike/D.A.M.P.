c028d5990309a03946dfc91966ac492c8af5e735
Peti <pmernyei@gmail.com>
2018-12-01 13:36:04 +0000

    Player stuff

---
+++ DAMP/Assets/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+	public void TakeDamage()
+	{
+		Debug.Log("Player hit!");
+	}
+}
