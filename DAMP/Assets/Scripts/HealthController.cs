57fd23d7094155fa3025071e24830b17474b46c5
Mike Cachopo <rui@cachopo.org>
2018-12-01 17:59:47 +0000

    Death now takes you to end menu. Also health disply. Also hook looks blue and pretty.

--- DAMP/Assets/Scripts/HealthController.cs
+++ DAMP/Assets/Scripts/HealthController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthController : MonoBehaviour
 {
@@ -8,16 +10,30 @@ public class HealthController : MonoBehaviour
 	public int hitPoints = 10;
 	public GameObject explosionEffect;
 	public GameObject hook;
-	
-	public void TakeDamage()
+    public TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        healthText.text = "Health: " + hitPoints;
+    }
+
+    public void TakeDamage()
 	{
 		hitPoints--;
+        healthText.text = "Health: " + hitPoints;
 		Debug.Log("Player hit! " + hitPoints + " HP left");
 		if (hitPoints == 0)
 		{
 			Instantiate(explosionEffect, transform.position, transform.rotation);
-			Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
 			Destroy(hook);
+            StartCoroutine(EndGame());
 		}
 	}
+
+    IEnumerator EndGame() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
